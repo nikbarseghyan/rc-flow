96cebeeeb95f7080366bf983155296677b51678a
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-04-30 13:52:41 +0400

    updaet

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './Service/serviceWorker';
 
- const root = document.getElementById('root');
+const root = document.getElementById('root');
 
 if( root != null ) {
     ReactDOM.render(<App />, root);
-    serviceWorker.unregister();
 }
-
-
